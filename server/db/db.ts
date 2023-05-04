543e848b91d1c084fdb3a87c8c87b3a3b0399285
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-04 14:50:39 +1200

    feat(server): add database function to get featured projects

--- server/db/db.ts
+++ server/db/db.ts
@@ -9,3 +9,7 @@ export function getAllProjects() {
 export function getProjectByName(name: string) {
   return db('projects').where({ name }).first()
 }
+
+export function getFeaturedProjects(featured: boolean) {
+  return db('projects').where({ featured })
+}
