fbbb118ada163cb5753512c7e7e1dbdd641872ab
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-05 21:56:23 +1200

    feat(client): add light and dark theme toggle

---
+++ client/components/LightDarkButton.tsx
@@ -0,0 +1,32 @@
+import { ActionIcon, useMantineColorScheme } from '@mantine/core'
+import { IconSun, IconMoonStars } from '@tabler/icons-react'
+
+export default function LightAndDarkButton() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+  const dark = colorScheme === 'dark'
+
+  return (
+    <ActionIcon
+      variant="outline"
+      color={dark ? 'yellow' : 'fawn'}
+      onClick={() => toggleColorScheme()}
+      title="Toggle color scheme"
+      sx={(theme) => ({
+        textAlign: 'center',
+        backgroundColor:
+          theme.colorScheme === 'dark'
+            ? theme.colors.yellow[0]
+            : theme.colors.yellow[1],
+        color:
+          theme.colorScheme === 'dark'
+            ? theme.colors.yellow[7]
+            : theme.colors.yellow[8],
+        borderColor: theme.colors.yellow[7],
+        borderRadius: theme.radius.md,
+        marginRight: theme.spacing.md,
+      })}
+    >
+      {dark ? <IconSun size="1.1rem" /> : <IconMoonStars size="1.1rem" />}
+    </ActionIcon>
+  )
+}
