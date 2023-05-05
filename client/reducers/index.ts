00c4c4dd249936d9bf245682f73232dbc726e6db
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-05 21:57:45 +1200

    feat(client): add allProjects reducer to combined reducers

--- client/reducers/index.ts
+++ client/reducers/index.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 
-// import stuff from './stuff'
+import allProjects from './allProjects'
 
 export default combineReducers({
-  // stuff
+  ProjectState: allProjects,
 })
