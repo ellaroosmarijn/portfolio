3ff6a9acf4201ce4e10b539f3e3015052e47cc23
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-06 14:15:09 +1200

    feat(client): create a ContentBox component to keep code dry

---
+++ client/components/ContentBox.tsx
@@ -0,0 +1,21 @@
+import { Box } from '@mantine/core'
+
+import { ChildrenProps } from '../../shared/types'
+
+export default function ContentBox({ children }: ChildrenProps) {
+  return (
+    <Box
+      sx={(theme) => ({
+        padding: theme.spacing.xl,
+        margin: theme.spacing.xl,
+        backgroundColor:
+          theme.colorScheme === 'dark'
+            ? theme.colors.yellow[9]
+            : theme.colors.yellow[0],
+        borderRadius: theme.radius.md,
+      })}
+    >
+      {children}
+    </Box>
+  )
+}
