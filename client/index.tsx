1e93e4d9caec1d520a3f3d018b814e156dd5f9bd
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-07 15:32:25 +1200

    fix(client): add BrowserRouter

--- client/index.tsx
+++ client/index.tsx
@@ -1,13 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import { Provider as ReduxProvider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import store from './store'
 
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <ReduxProvider store={store}>
-      <App />
-    </ReduxProvider>
+    <BrowserRouter>
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
+    </BrowserRouter>
   )
 })
