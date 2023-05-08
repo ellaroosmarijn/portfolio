2a9cbbbb44e646f50c203c1c10bce95c02ebb0ff
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-08 13:38:08 +1200

    style(client): add enum ComponentBoxVariant and props - remove light/dark theme

--- client/components/ContentBox.tsx
+++ client/components/ContentBox.tsx
@@ -2,18 +2,29 @@ import { Box } from '@mantine/core'
 
 import { ChildrenProps } from '../../shared/types'
 
-export default function ContentBox({ children }: ChildrenProps) {
+export enum ContentBoxVariant {
+  Dark,
+  Light,
+}
+interface ContentBoxProps extends ChildrenProps {
+  variant?: ContentBoxVariant
+}
+
+export default function ContentBox({
+  children,
+  variant = ContentBoxVariant.Dark,
+}: ContentBoxProps) {
   return (
     <Box
+      m={0}
+      py={'300px'}
       sx={(theme) => ({
         textAlign: 'center',
         padding: theme.spacing.xl,
         margin: theme.spacing.xl,
         backgroundColor:
-          theme.colorScheme === 'dark'
-            ? theme.colors.yellow[9]
-            : theme.colors.yellow[0],
-        borderRadius: theme.radius.md,
+          variant === ContentBoxVariant.Dark ? theme.black : theme.white,
+        color: variant === ContentBoxVariant.Dark ? theme.white : theme.black,
       })}
     >
       {children}
