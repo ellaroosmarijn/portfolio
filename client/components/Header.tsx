34795907f8b5b125a77f3eb048f2439306343e4b
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-10 23:35:25 +1200

    feat(client): add clickable CV and LinkedIn icons to header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,6 +1,8 @@
-import { Header, Group } from '@mantine/core'
+import { Header, Group, ThemeIcon } from '@mantine/core'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import styled from '@emotion/styled'
+import { GoFilePdf } from 'react-icons/go'
+import { FaLinkedin } from 'react-icons/fa'
 
 const StyledAnchorLink = styled(AnchorLink)`
   text-align: center;
@@ -19,12 +21,21 @@ const StyledAnchorLink = styled(AnchorLink)`
 `
 
 export default function HeaderDiv(): JSX.Element {
+  const iconStyle = {
+    color: 'white',
+    backgroundColor: 'black',
+    maxHeight: '20px',
+  }
+
   return (
     <Header
       height={{ base: 50, md: 70 }}
       sx={(theme) => ({
         padding: theme.spacing.xl,
         mixBlendMode: 'difference',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       })}
     >
       <Group position="center" spacing="100px">
@@ -37,6 +48,23 @@ export default function HeaderDiv(): JSX.Element {
         <StyledAnchorLink href="#contact" data-hover="contact" offset={200}>
           Contact
         </StyledAnchorLink>
+      </Group>
+      <Group
+        sx={() => ({ position: 'absolute', right: '1em' })}
+        position="center"
+        spacing="10px"
+        align="center"
+      >
+        <a href="https://www.linkedin.com/in/ellarrobinson/">
+          <ThemeIcon variant="light">
+            <FaLinkedin style={iconStyle} />
+          </ThemeIcon>
+        </a>
+        <a href="EllaRobinson_CV.pdf">
+          <ThemeIcon variant="light">
+            <GoFilePdf style={iconStyle} />
+          </ThemeIcon>
+        </a>
       </Group>
     </Header>
   )
