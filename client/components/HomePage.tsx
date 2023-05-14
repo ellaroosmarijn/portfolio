fe6b25a04ed7b9afc208e638b882b92ca803eff0
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-14 13:47:34 +1200

    refactor(client): make code drier by removing font-size and media queries - now added to BaseTitle

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -1,38 +1,23 @@
-import styled from '@emotion/styled'
-
 import AboveTitleText from './AboveTitleText'
 import BaseTitle from './BaseTitle'
 import ContentBox, { ContentBoxVariant } from './ContentBox'
 import BelowTitleText from './BelowTitleText'
-
-const Title = styled(BaseTitle)`
-    font-size: 12.5rem;
-
-    @media (max-width: 1150px) {
-      font-size: 10rem;
-    }
-    @media (max-width: 907px) {
-      font-size: 7.5rem;
-    }
-    @media (max-width: 677px) {
-      font-size: 5.5rem;
-    }
-    @media (max-width: 530px) {
-      font-size: 3.5rem;
-    }
-  }
-`
+import HideMobile from './HideMobile'
 
 export default function HomePage() {
   return (
     <>
       <ContentBox variant={ContentBoxVariant.Dark}>
         <AboveTitleText>I AM</AboveTitleText>
-        <Title>ELLA ROOSMARIJN</Title>
+        <BaseTitle>ELLA ROOSMARIJN</BaseTitle>
         <BelowTitleText>
           A GRADUATE SOFTWARE DEVELOPER LOOKING TO HAVE AN OUTSIZED IMPACT ON
-          THE WORLD. INTERESTED IN POLITICS, PHILOSOPHY, ECONOMICS, AND BUILDING
-          CREATIVE THINGS.
+          THE WORLD.
+          <HideMobile>
+            {' '}
+            INTERESTED IN POLITICS, PHILOSOPHY, ECONOMICS, AND BUILDING CREATIVE
+            THINGS.
+          </HideMobile>
         </BelowTitleText>
       </ContentBox>
     </>
