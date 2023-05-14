77f0d46eb941f488c721c680b4d83731633951cc
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-14 12:36:43 +1200

    refactor(client): refactor code to create BelowTitleText component so it's styling is reusable

---
+++ client/components/BelowTitleText.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+
+const BelowTitleText = styled.div`
+  font-size: 1rem;
+  font-weight: 600;
+  whitespace: 'pre-line';
+  line-height: 0.9;
+  letter-spacing: -0.05em;
+`
+
+export default BelowTitleText
