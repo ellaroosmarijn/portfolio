463a4adbccc65813d80e0c87deccfaffdb8209f6
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-14 12:37:33 +1200

    refactor(client): refactor Homepage component to use emotion css

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -1,31 +1,39 @@
-import { Container, Text, Title } from '@mantine/core'
+import styled from '@emotion/styled'
 
-import BigTitle from './BigTitle'
-import ContentBox from './ContentBox'
+import AboveTitleText from './AboveTitleText'
+import BaseTitle from './BaseTitle'
+import ContentBox, { ContentBoxVariant } from './ContentBox'
+import BelowTitleText from './BelowTitleText'
+
+const Title = styled(BaseTitle)`
+    font-size: 12.5rem;
+
+    @media (max-width: 1150px) {
+      font-size: 10rem;
+    }
+    @media (max-width: 907px) {
+      font-size: 7.5rem;
+    }
+    @media (max-width: 677px) {
+      font-size: 5.5rem;
+    }
+    @media (max-width: 530px) {
+      font-size: 3.5rem;
+    }
+  }
+`
 
 export default function HomePage() {
   return (
     <>
-      <ContentBox>
-        <Container
-          sx={(theme) => ({
-            maxWidth: '1150px',
-            [`@media (max-width: ${theme.breakpoints.xs})`]: {
-              paddingLeft: '0',
-              paddingRight: '0',
-            },
-          })}
-        >
-          <Text fz="sm" fw="700">
-            I AM
-          </Text>
-          <BigTitle order={1}>ELLA ROOSMARIJN</BigTitle>
-          <Text fz="xs" fw="600" sx={() => ({ whiteSpace: 'pre-line' })}>
-            A GRADUATE SOFTWARE DEVELOPER LOOKING TO HAVE AN OUTSIZED IMPACT ON
-            THE WORLD. INTERESTED IN POLITICS, PHILOSOPHY, ECONOMICS, AND
-            BUILDING CREATIVE THINGS.
-          </Text>
-        </Container>
+      <ContentBox variant={ContentBoxVariant.Dark}>
+        <AboveTitleText>I AM</AboveTitleText>
+        <Title>ELLA ROOSMARIJN</Title>
+        <BelowTitleText>
+          A GRADUATE SOFTWARE DEVELOPER LOOKING TO HAVE AN OUTSIZED IMPACT ON
+          THE WORLD. INTERESTED IN POLITICS, PHILOSOPHY, ECONOMICS, AND BUILDING
+          CREATIVE THINGS.
+        </BelowTitleText>
       </ContentBox>
     </>
   )
