dc02db2c2ce0cdc0a323da70ddede34c001e930e
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-14 12:39:19 +1200

    fix(client): add id to the ContentBoxProps - id needed so header's smooth scroll works

--- client/components/ContentBox.tsx
+++ client/components/ContentBox.tsx
@@ -7,6 +7,7 @@ export enum ContentBoxVariant {
 }
 interface ContentBoxProps extends ChildrenProps {
   variant: ContentBoxVariant
+  id?: string
 }
 
 const ContentBoxWrapper = styled.div<ContentBoxProps>`
