916202d6594fa2e1b4537ce9542ca9c8b84b3197
Ella Robinson <ellaroosmarijn@gmail.com>
2023-05-14 12:15:23 +1200

    style(client): add BaseTitle component so the styling can be reused

---
+++ client/components/BaseTitle.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+
+const BaseTitle = styled.div`
+  white-space: nowrap;
+  font-family: 'Vina Sans';
+  margin-top: -0.1em;
+  padding-bottom: 0.1em;
+  font-weight: 500;
+  line-height: 0.75;
+`
+
+export default BaseTitle
